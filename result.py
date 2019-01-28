4f33b533c4aa586c43847d2a7e24543261d91cac
lilei.feng <lilei.feng@pku.edu.cn>
2019-01-28 23:55:15 +0800

    result.py beta version

--- result.py
+++ result.py
@@ -37,14 +37,14 @@ def list_average(num):
 
 
 
-def fetch_result(random, bps, lane, velocity, spacing):
+def fetch_result(random, bps, signtype,lane, velocity, spacing):
     res_RTB = []
     res_RDB = []
     res_fail_count = 0
     res_item_count = 0
 #    dir_name = "lane"+str(lane)+"_velocity"+str(velocity)+"_spacing"+str(spacing)
 
-    key = "random"+str(random)+"_bps"+str(bps)+"_lane"+str(lane)+"_velocity"+str(velocity)+"_spacing"+str(spacing)
+    key = "random"+str(random)+"_bps"+str(bps)+"_signtype"+str(signtype)+"_lane"+str(lane)+"_velocity"+str(velocity)+"_spacing"+str(spacing)
     suffix = ".csv"
     fname = key+suffix
 
@@ -59,33 +59,89 @@ def fetch_result(random, bps, lane, velocity, spacing):
                 res_item_count = res_item_count + item_count
     return list_average(res_RTB), list_average(res_RDB),res_fail_count, res_item_count
 
-velocity = [30, 50, 70]
-spacing = [100, 500, 1000]
-
-random_tbl = [0, 1]
-bps_tbl = [128, 256]
 
 '''
 fetch_data("/home/llfeng/VLC/simulator/RetroI2V_MAC/USE_ROUND/root/lane1_velocity2_spacing100/result-2019-01-26_23_35_39_lane1_velocity2_spacing100.csv")
 
 '''
-ave_RDB = 0.0
-fail_count = 0
-total_count = 0
-res_dict = {}
-for random in random_tbl:
-    for bps in bps_tbl:
-        for i in range(1, 4):   #lane: 1,2,3
-            for j in range (0, 3):  #velocity: 0,1,2
-                for k in spacing:
-                    item_key = "random"+random+"_bps"+bps+"_lane"+str(i)+"_velocity"+str(velocity[j])+"_spacing"+str(k)
-                    item_val = fetch_result(random,bps,i,j,k)
-                    res_dict[item_key] = item_val
+def gen_res_dict()
+    para_tbl = {
+        "random": [0,1],
+        "bps": [128, 256],
+        "signtype": [1,2],
+        "lane": [1,2,3]
+        "velocity": [30,50,70],
+        "spacing": [100,500,1000]
+    }
 
-data=DataFrame(res_dict)
-data.to_csv("data.csv", index=False)
-print data
+#    velocity = [30, 50, 70]
+#    spacing = [100, 500, 1000]
 
+#    random_tbl = [0, 1]
+#    bps_tbl = [128, 256]
+    res_dict = {}
+    for random in para_tbl["random"]:
+        for bps in para_tbl["bps"]:
+            for signtype in para_tbl["signtype"]:
+            	for lane in para_tbl["lane"]:   #lane: 1,2,3
+            	    for j in range (0, len(para_tbl["velocity"])):  #velocity: 0,1,2
+            	        for spacing in para_tbl["spacing"]:
+            	            item_key = "random"+random+"_bps"+bps+"_signtype"+signtype+"_lane"+lane+"_velocity"+para_tbl["velocity"][j]+"_spacing"+spacing
+            	            item_val = fetch_result(random,bps, signtype,lane,j,spacing)
+            	            res_dict[item_key] = item_val   #[RTB, RDB, fail_count, res_item_count]
+
+    data=DataFrame(res_dict)
+    data.to_csv("parse_data.csv", index=False)
+    print data
+    return res_dict
+
+
+
+def parse_plot_config(config_item_tbl, res_dict)
+    para_tbl = {
+        "random": [0,1],
+        "bps": [128, 256],
+        "signtype": [1,2],
+        "lane": [1,2,3]
+        "velocity": [30,50,70],
+        "spacing": [100,500,1000]
+    }
+
+    plot_dict = {}
+    config_count=0
+    for config in config_item_tbl:
+        for ele_val in para_tbl["spacing"]:
+            key_name = "random" + config["random"] + "_bps" + config["bps"] + "_signtype" + config["signtype"] + "_lane" + config["lane"] + "_velocity" + config["velocity"] + "_spacing" + ele_val 
+            plot_dict = plot_dict + res_dict[key_name];
+    return plot_dict
+
+def gen_plot_data()
+    plot_config_tbl = {
+        'work_range':[  
+                        {"random":0, "bps":128, "signtype":1, "lane":2, "velocity":50, "spacing":-1}
+                        {"random":0, "bps":128, "signtype":2, "lane":2, "velocity":50, "spacing":-1}
+                        {"random":0, "bps":256, "signtype":1, "lane":2, "velocity":50, "spacing":-1}
+                        {"random":0, "bps":256, "signtype":2, "lane":2, "velocity":50, "spacing":-1}
+                    ],
+        'road_traffic':[
+                        {"random":0, "bps":128, "signtype":2, "lane":1, "velocity":30, "spacing":-1}, 
+                        {"random":0, "bps":128, "signtype":2, "lane":2, "velocity":50, "spacing":-1}, 
+                        {"random":0, "bps":128, "signtype":2, "lane":3, "velocity":70, "spacing":-1}
+                    ],
+        'tag_density':[
+                        {"random":1, "bps":128, "signtype":2, "lane":1, "velocity":50, "spacing":-1}
+                        {"random":1, "bps":128, "signtype":2, "lane":2, "velocity":50, "spacing":-1}
+                        {"random":1, "bps":128, "signtype":2, "lane":3, "velocity":50, "spacing":-1}
+                    ]
+    }
+    plot_res_dict = {}
+    data_res_dict = gen_res_dict()
+    for key, val in plot_config_tbl:
+        plot_res_dict[key] = parse_plot_config(val, data_res_dict)
+
+    print data_res_dict
+    data = DataFrame(plot_res_dict)
+    data.to_csv("plot_data.csv", index=False)
 
 
 '''
